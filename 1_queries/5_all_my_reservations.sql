1e96afdfa5d0eb69e046c0fa4e1d782490f6a97c
Joseph Micla <josephmicla@gmail.com>
2021-11-24 18:22:21 -0800

    creates query to find users reservations

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,8 @@
+SELECT reservations.*, properties.*, AVG(rating)
+FROM reservations
+JOIN properties ON properties.id = property_id
+JOIN property_reviews ON reservations.id = reservation_id
+WHERE reservations.guest_id = 1
+AND end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY start_date DESC;
